1d4e56cb1e2a7c52e5ed75d2f0ddac7a7968e3a4
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-05-30 09:19:00 -0300

    Tratando Exceptions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,14 +1,35 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaAny
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
     println("início main")
+
+    val entrada: String = "1.9"
+    val valorConvertido: Double? = try {
+        entrada.toDouble()
+    }catch (e: NumberFormatException){
+        println("Problema na conversão")
+        null
+    }
+
+    val valorComTaxa: Double? = if(valorConvertido != null){
+        valorConvertido + 0.10
+    }else{
+        null
+    }
+
+    if(valorComTaxa != null){
+        println("O valor com taxa $valorComTaxa")
+    }else{
+        println("Valor ficou nulo")
+    }
+
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
     funcao2()
     println("fim funcao1")
@@ -16,8 +37,15 @@ fun funcao1(){
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+    for (i in 1..5) {
         println(i)
+    }
+    try {
+        val endereco = Any()
+        endereco as Endereco
+    } catch (e: ClassCastException) {
+        println("ClassCastException foi pegada")
+        e.printStackTrace()
     }
     println("fim funcao2")
 }
